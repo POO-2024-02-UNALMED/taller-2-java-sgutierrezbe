2ed235897e04bfe0fa77a948a4e76f9090acd9ae
Samuel Gutierrez <sgutierrezbe@unal.edu.co>
2024-11-11 11:43:45 -0500

    fix#1

--- src/main/java/Auto.java
+++ src/main/java/Auto.java
@@ -18,6 +18,10 @@ public class Auto {
         cantidadCreados++;
     }
 
+    public Auto() {
+      
+    }
+
     public int cantidadAsientos() {
         int count = 0;
         for (Asiento asiento : asientos) {
@@ -48,6 +52,54 @@ public class Auto {
 
     public static int getCantidadCreados() {
         return cantidadCreados;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Asiento[] getAsientos() {
+        return asientos;
+    }
+
+    public void setAsientos(Asiento[] asientos) {
+        this.asientos = asientos;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public void setMotor(Motor motor) {
+        this.motor = motor;
+    }
+
+    public int getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(int registro) {
+        this.registro = registro;
     }
 }
 
